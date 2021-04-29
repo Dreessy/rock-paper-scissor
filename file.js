a9a8f6fc01190583d83af9223f0a2e24977c3799
Dreessy <thevideogameitalia@gmail.com>
2021-04-29 09:46:23 +0200

    Rock Paper Scissor updated with javascript file

--- file.js
+++ file.js
@@ -1,77 +1,91 @@
+let player = 0;
+let computer = 0;
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissor = document.querySelector("#scissor");
+const risults = document.querySelector("#risults");
+
+rock.addEventListener("click", () => { playerRound(rock.id) });
+paper.addEventListener("click", () => { playerRound(paper.id) });
+scissor.addEventListener("click", () => { playerRound(scissor.id) });
+
+risults.textContent = player + " - " + computer;
+
 function computerPlay() {
     let computerValue = Math.floor(Math.random() * 3);
     return computerValue;
 }
-function playerPlay() {
-    let playerValue = prompt("Choose! Rock,Paper,Scissor");
-    playerValue = playerValue.toLowerCase();
+function playerPlay(playerValue) {
     switch (playerValue) {
         case ("rock"):
+            console.log("rock");
             return 0;
         case ("paper"):
+            console.log("paper");
             return 1;
         case ("scissor"):
+            console.log("scissor");
             return 2;
-        default:
-            alert("Insert a value!");
-            playerPlay();
     }
 }
-function playerRound() {
+function playerRound(selection) {
     let computerSelection = computerPlay();
-    let playerSelection = playerPlay();
+    let playerSelection = playerPlay(selection);
     if (playerSelection == 0 && computerSelection == 0) {
         console.log("Draw")
-        return 0;
+
     }
     else if (playerSelection == 1 && computerSelection == 1) {
         console.log("Draw")
-        return 0;
+
     }
     else if (playerSelection == 2 && computerSelection == 2) {
         console.log("Draw")
-        return 0;
+
     }
     else if (playerSelection == 0 && computerSelection == 1) {
         console.log("Computer Win")
-        return 2;
+        computer++;
+
     }
 
     else if (playerSelection == 0 && computerSelection == 2) {
         console.log("Computer win")
-        return 2;
+        computer++;
     }
     else if (playerSelection == 1 && computerSelection == 0) {
         console.log("Player win")
-        return 1;
+        computer++;
+
     }
     else if (playerSelection == 1 && computerSelection == 2) {
         console.log("Computer win")
-        return 2;
+        computer++;
+
     }
     else if (playerSelection == 2 && computerSelection == 0) {
         console.log("Computer win")
-        return 2;
+        computer++;
+
     }
     else if (playerSelection == 2 && computerSelection == 1) {
         console.log("Player win")
-        return 1;
+        player++;
+
+    }
+    risults.textContent = player + " - " + computer;
+    if (player === 5) {
+        alert("player win the game");
+        player = 0;
+        compuiter = 0;
+        risults.textContent = player + " - " + computer;
+    }
+    else if (computer === 5) {
+        alert("Computer win the game");
+        player = 0;
+        computer = 0;
+        risults.textContent = player + " - " + computer;
     }
 }
-function game() {
-    let round = 1;
-    let playerWins = 0;
-    let computerWins = 0;
-    while (round <= 5) {
-        let result = playerRound();
-        if (result === 1) {
-            playerWins++;
-        }
-        else if (result === 2) {
-            computerWins++;
-        }
-        console.log("Player " + playerWins + " - " + "Computer" + computerWins);
-        round++;
-    }
-}
-game();
+
+
